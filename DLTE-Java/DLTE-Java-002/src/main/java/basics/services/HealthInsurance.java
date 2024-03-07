86acd1f9399adc631ca3d3fd116155fbe4f2a73a
PRASHA02 <prashanthd2002@gmail.com>
2024-03-07 11:49:58 +0530

    added command line argument

--- DLTE-Java/DLTE-Java-002/src/main/java/basics/services/HealthInsurance.java
+++ DLTE-Java/DLTE-Java-002/src/main/java/basics/services/HealthInsurance.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class HealthInsurance {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        if (args.length == 0) {
+            System.out.println("Usage: java HealthInsurance <insurance_type>");
+            return;
+        }
         String[] health = {"health","life","medicine"};
-        System.out.println("Enter your health insurance :");
-        String matchingString = scanner.nextLine();
-        for(int i=0;i<health.length;i++){
-            if(health[i].equalsIgnoreCase(matchingString)){
-                System.out.println("Your health insurance position is "+i);
-                scanner.close();
+        String matchingString = args[0];
+        for (int i = 0; i < health.length; i++) {
+            if (health[i].equalsIgnoreCase(matchingString)) {
+                System.out.println("Your health insurance position is " + i);
                 return;
             }
         }
         System.out.println("Health insurance position is not found");
-        scanner.close();
     }
 }
