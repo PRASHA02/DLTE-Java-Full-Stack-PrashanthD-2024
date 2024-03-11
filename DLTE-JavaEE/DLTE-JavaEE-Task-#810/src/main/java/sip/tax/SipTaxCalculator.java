b26312916cfebbc9f8c7a6e94f49e335ae013c72
PRASHA02 <prashanthd2002@gmail.com>
2024-03-11 23:54:09 +0530

    task completed #818

--- DLTE-JavaEE/DLTE-JavaEE-Task-#810/src/main/java/sip/tax/SipTaxCalculator.java
+++ DLTE-JavaEE/DLTE-JavaEE-Task-#810/src/main/java/sip/tax/SipTaxCalculator.java
@@ -14,23 +14,7 @@ public class SipTaxCalculator extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-          //        super.doGet(req, resp);
-//        int principle = Integer.parseInt(req.getParameter("principle"));
-//        Double interestRate = Double.parseDouble(req.getParameter("interestRate"));
-//        int period = Integer.parseInt(req.getParameter("period"));
-//        Double estimatedAmount = 0.0,totalAmount=0.0;
-//        interestRate = interestRate / (12*100);
-//        period *=12;
-//        totalAmount = (principle*((Math.pow(1+interestRate,period)-1)/interestRate)*(1+interestRate));
-//        estimatedAmount = totalAmount - principle;
-//        resp.setContentType("application/json");
-//        Gson gson = new Gson();
-//        String totalAmountMessage = gson.toJson(totalAmount);
-//        resp.getWriter().println("The total Amount = "+totalAmountMessage);
-//        String estimatedAmountMessage = gson.toJson(estimatedAmount);
-//        resp.getWriter().println("Estimated Amount = "+estimatedAmountMessage);
-          resp.getWriter().println("hello");
+        resp.getWriter().println("hello");
     }
-
 
 }
