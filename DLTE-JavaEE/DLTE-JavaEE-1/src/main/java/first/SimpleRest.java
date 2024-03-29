ac6354b687c79856c5f35ea10245fdad2cb1df57
PRASHA02 <prashanthd2002@gmail.com>
2024-03-29 14:26:04 +0530

    updated security in resr services

--- DLTE-JavaEE/DLTE-JavaEE-1/src/main/java/first/SimpleRest.java
+++ DLTE-JavaEE/DLTE-JavaEE-1/src/main/java/first/SimpleRest.java
@@ -1,36 +1,36 @@
-//package first;
-////
-//import com.google.gson.Gson;
-//import soap.services.GroupTransaction;
-//import soap.services.TransactionService;
+package first;
 //
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.*;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//import java.util.Date;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//import java.util.stream.Stream;
-//
-//@WebServlet("/simple")
-//public class SimpleRest extends HttpServlet {
-//    List<Transaction> transactionList = Stream.of(
-//            new Transaction(new Date(2024, 11, 9), 10000.0, "vineeth", "Family"),
-//            new Transaction(new Date(2025, 02, 19), 50000.0, "Elroy", "Education"),
-//            new Transaction(new Date(2022, 12, 18), 10000.0, "Varun", "Bills"),
-//            new Transaction(new Date(2025, 06, 5), 10000.0, "Vignesh", "Emergency"),
-//            new Transaction(new Date(2024, 2, 20), 20000.0, "Shreyas", "Friends")
-//    ).collect(Collectors.toList());
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//        // Serialize transactionList to JSON
-//        Gson gson = new Gson();
-//        String transactionListJson = gson.toJson(transactionList);
-//
+import com.google.gson.Gson;
+import soap.services.GroupTransaction;
+import soap.services.TransactionService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@WebServlet("/simple")
+public class SimpleRest extends HttpServlet {
+    List<Transaction> transactionList = Stream.of(
+            new Transaction(new Date(2024, 11, 9), 10000.0, "vineeth", "Family"),
+            new Transaction(new Date(2025, 02, 19), 50000.0, "Elroy", "Education"),
+            new Transaction(new Date(2022, 12, 18), 10000.0, "Varun", "Bills"),
+            new Transaction(new Date(2025, 06, 5), 10000.0, "Vignesh", "Emergency"),
+            new Transaction(new Date(2024, 2, 20), 20000.0, "Shreyas", "Friends")
+    ).collect(Collectors.toList());
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // Serialize transactionList to JSON
+        Gson gson = new Gson();
+        String transactionListJson = gson.toJson(transactionList);
+
 //        // SOAP endpoint URL
 //        String soapEndpointUrl = "http://localhost:8083/soap/transactions";
 //
@@ -57,8 +57,8 @@
 //        String transactionJson = gson.toJson(groupTransaction);
 //        resp.setStatus(HttpServletResponse.SC_OK);
 //        resp.getWriter().println(transactionJson);
-//
-//}
-//}
-//
-//
+
+}
+}
+
+
