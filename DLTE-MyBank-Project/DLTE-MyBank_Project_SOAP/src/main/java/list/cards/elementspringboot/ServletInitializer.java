9974e69e596a70aebd9dbd7efbde100f53bc9877
PRASHA02 <prashanthd2002@gmail.com>
2024-04-03 16:13:02 +0530

    updated the mybank database of final project

---
+++ DLTE-MyBank-Project/DLTE-MyBank_Project_SOAP/src/main/java/list/cards/elementspringboot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package list.cards.elementspringboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ElementSpringBootApplication.class);
+    }
+
+}
---
+++ DLTE-MyBank-Project/DLTE-MyBank_Project_SOAP/src/test/java/list/cards/elementspringboot/ElementSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package list.cards.elementspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElementSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
