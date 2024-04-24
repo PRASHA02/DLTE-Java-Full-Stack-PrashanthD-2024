a3d78850a00c40bf08363412fd7386c514885638
PRASHA02 <prashanthd2002@gmail.com>
2024-04-24 15:32:02 +0530

    updated thymleaf task 1681

--- DLTE-Java-Spring/DLTE-JavaSpring-tasks/DLTE-Java-Spring-Thymeleaf-1681/src/main/java/com/example/elementdashboard/WebController.java
+++ DLTE-Java-Spring/DLTE-JavaSpring-tasks/DLTE-Java-Spring-Thymeleaf-1681/src/main/java/com/example/elementdashboard/WebController.java
@@ -5,14 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 //controller for thymeleaf html pages
 @Controller
 public class WebController {
+    //dashboard
     @GetMapping("/")
     public String index(){
         return "index";
     }
+    //displaying the cards
     @GetMapping("/soap")
     public String soap(){
         return "soap";
     }
+    //updating the cards
     @GetMapping("/rest")
     public String rest(){
         return "rest";
